198ef05afc232195d67f412946e1ec33305fb30f
nekowrk <neko.wrk@gmail.com>
2023-11-19 15:12:27 +0200

    fixed test var format

--- tests/test_cubeit.py
+++ tests/test_cubeit.py
@@ -15,12 +15,12 @@ BASE_URL = "http://127.0.0.1:8000/"
 def test_cube(page: Page, num, num_cube):
     page.goto(BASE_URL)
 
-    page.get_by_placeholder("enter number...").fill(num)
+    page.get_by_placeholder("enter number...").fill(str(num))
     page.get_by_role("button", name="Cube").click()
 
     result = page.locator("css=p#result")
 
-    expect(result).to_contain_text(num_cube)
+    expect(result).to_contain_text(str(num_cube))
 
 def test_empty_input(page: Page):
     page.goto(BASE_URL)
