e0606d8937e3cf0e199ca4f1770b1eb2f574c5be
nekowrk <neko.wrk@gmail.com>
2023-11-19 15:09:33 +0200

    added parametrize

--- tests/test_cubeit.py
+++ tests/test_cubeit.py
@@ -1,17 +1,26 @@
 from playwright.sync_api import Page, expect
+import pytest
 
 
 BASE_URL = "http://127.0.0.1:8000/"
 
-def test_cube(page: Page):
+@pytest.mark.parametrize(
+    ("num", "num_cube"),
+    (
+    (2, 8),
+    (3, 27),
+    (5, 125)
+    )
+)
+def test_cube(page: Page, num, num_cube):
     page.goto(BASE_URL)
 
-    page.get_by_placeholder("enter number...").fill("5")
+    page.get_by_placeholder("enter number...").fill(num)
     page.get_by_role("button", name="Cube").click()
 
     result = page.locator("css=p#result")
 
-    expect(result).to_contain_text("125")
+    expect(result).to_contain_text(num_cube)
 
 def test_empty_input(page: Page):
     page.goto(BASE_URL)
